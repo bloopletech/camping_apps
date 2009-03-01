1f66b312d9367c656588b32367319c9126438e58
Brenton Fletcher <bloopletech@BrentonsComputer.local>
2009-03-01 17:57:18 +1030

    Changes

---
+++ lib/acts-as-taggable-on/init.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + "/models"
+require File.dirname(__FILE__) + "/acts_as_taggable_on"
+require File.dirname(__FILE__) + "/acts_as_tagger"
+require File.dirname(__FILE__) + "/tags_helper"
