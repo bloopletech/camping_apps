98009370d85cc4f23b175fdd764b4f4c03802800
Brenton Fletcher <bloopletech@BrentonsComputer.local>
2009-05-29 21:10:27 +0930

    Add to unicode.rb

--- unicode.rb
+++ unicode.rb
@@ -6,17 +6,28 @@ Camping.goes :Unicode
 
 module Unicode
   PATH = File.expand_path(File.dirname(__FILE__)) + '/unicode'
+  def self.load_mapping
+    if $mapping.empty?
+      IO.readlines(Unicode::PATH + "/unicode.txt").each do |line|
+        code_point, name = line.strip.split("\t")
+        $mapping[code_point] = name
+      end
+    end
+  end
 end
 
 module Unicode::Controllers
   class Index < R '/'
     def get
-      if $mapping.empty?
-        IO.readlines(Unicode::PATH + "/unicode.txt").each do |line|
-          code_point, name = line.strip.split("\t")
-          $mapping[code_point] = name
-        end
-      end
+      Unicode.load_mapping
+      @code_point = $mapping.keys[rand($mapping.keys.length)]
+      render :index
+    end
+  end
+  class Single < R '/([A-z0-9]+)'
+    def get(code_point)
+      Unicode.load_mapping
+      @code_point = code_point.upcase
       render :index
     end
   end
@@ -44,16 +55,15 @@ module Unicode::Views
       end
       body do
         self << yield
-        p.footer! { "Created by #{a "Brenton Fletcher", :href => "http://i.bloople.net"} in #{File.stat(__FILE__).size} bytes of code. <a href='mailto:&#105;&#064;&#098;&#108;&#111;&#111;&#112;&#108;&#101;&#046;&#110;&#101;&#116;'>Email me</a>! Made on a #{a "Mac", :href => "http://apple.com"}. Powered by #{a "Ruby", :href => "http://rubyonrails.org"} on #{a "Camping", :href => "http://code.whytheluckystiff.net/camping/"}." }
+        p.footer! { "Created by #{a "Brenton Fletcher", :href => "http://i.bloople.net"} in #{File.stat(__FILE__).size} bytes of code. <a href='mailto:&#105;&#064;&#098;&#108;&#111;&#111;&#112;&#108;&#101;&#046;&#110;&#101;&#116;'>Email me</a>! Powered by #{a "Ruby", :href => "http://rubyonrails.org"} on #{a "Camping", :href => "http://code.whytheluckystiff.net/camping/"}." }
       end
     end
   end
 
   def index
     h1 { "Random Unicode Character" }
-    code_point = $mapping.keys[rand($mapping.keys.length)]
-    p.code_point! { "&#x#{code_point};" }
-    p.name! { "#{$mapping[code_point]}" }
+    p.code_point! { "&#x#{@code_point};" }
+    p.name! { "#{$mapping[@code_point]}" }
     p.note! { "Does not include the ranges CJK Ideograph Extension A, CJK Ideograph Extension B, CJK Ideograph, Hangul Syllable, Non Private Use High Surrogate, Private Use High Surrogate, Low Surrogate, High Surrogate, Private Use, Plane 15 Private Use, Plane 16 Private Use." }
   end
 
