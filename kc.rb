3765f20f19c5a51fa5ddd830340f097aa11191c3
Brenton Fletcher <bloopletech@BrentonsComputer.local>
2009-08-26 20:37:41 +0930

    Fix bugs in kc

--- kc.rb
+++ kc.rb
@@ -17,6 +17,33 @@ end
 
 module Kc::Models
   include KcModels
+
+  class CreateKc < V 6
+    def self.up
+      create_table "kc_scores", :force => false do |t|
+        t.integer  "version", "user_id", "score"
+        t.datetime "when"
+        t.string   "source"
+      end
+
+      create_table "kc_shouts", :force => false do |t|
+        t.string   "username", "text"
+        t.datetime "posted"
+      end
+
+      create_table "kc_users", :force => false do |t|
+        t.string  "name", "crypt"
+        t.integer "high_score"
+        t.integer :view_count, :default => 0
+        t.boolean :seen_site_changes_12_2008, :default => false
+        t.datetime :scores_when, :default => nil
+        t.integer :top_score_id
+        t.integer :latest_score_id
+        t.integer :total_scores
+        t.boolean "has_avatar", :default => false
+      end
+    end
+  end
 end
 
 module Kc::Controllers
--- kc/models.rb
+++ kc/models.rb
@@ -7,7 +7,7 @@ module KcModels
         after_save :update_high_scores
 
         def update_high_scores
-          hs = user.scores.find(:all, :order => "akc_scores.when DESC", :limit => 100)
+          hs = user.scores.find(:all, :order => "\#{self.table_name}.when DESC", :limit => 100)
 
           user.update_attributes(:high_score => hs.empty? ? 0 : ((hs.inject(0) { |sum, val| sum + val.score }) / hs.length.to_f).round, :latest_score_id => self.id, :top_score_id => user.scores.find(:first, :order => 'score DESC').id, :total_scores => user.scores.count)
         end
@@ -39,34 +39,7 @@ module KcModels
         end
 
         def most_recent_score
-          scores.find(:first, :order => 'akc_scores.when DESC')
-        end
-      end
-
-      class CreateAkc < V 6
-        def self.up
-          create_table "akc_scores", :force => false do |t|
-            t.integer  "version", "user_id", "score"
-            t.datetime "when"
-            t.string   "source"
-          end
-
-          create_table "akc_shouts", :force => false do |t|
-            t.string   "username", "text"
-            t.datetime "posted"
-          end
-
-          create_table "akc_users", :force => false do |t|
-            t.string  "name", "crypt"
-            t.integer "high_score"
-            t.integer :view_count, :default => 0
-            t.boolean :seen_site_changes_12_2008, :default => false
-            t.datetime :scores_when, :default => nil
-            t.integer :top_score_id
-            t.integer :latest_score_id
-            t.integer :total_scores
-            t.boolean "has_avatar", :default => false
-          end
+          scores.find(:first, :order => '\#{self.table_name}.when DESC')
         end
       end
     EOF
